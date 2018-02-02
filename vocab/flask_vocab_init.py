1fb78d0d319f1bf5729e8817db355fac1a6dcd0b
Jim Li <jinjiel1994@gmail.com>
2018-02-01 23:03:11 -0800

    Implement .py

---
+++ vocab/flask_vocab_init.py
@@ -0,0 +1,183 @@
+"""
+Flask web site with vocabulary matching game
+(identify vocabulary words that can be made 
+from a scrambled string)
+"""
+
+import flask
+import logging
+
+# Our own modules
+from letterbag import LetterBag
+from vocab import Vocab
+from jumble import jumbled
+import config
+
+###
+# Globals
+###
+app = flask.Flask(__name__)
+
+CONFIG = config.configuration()
+app.secret_key = CONFIG.SECRET_KEY  # Should allow using session variables
+
+#
+# One shared 'Vocab' object, read-only after initialization,
+# shared by all threads and instances.  Otherwise we would have to
+# store it in the browser and transmit it on each request/response cycle,
+# or else read it from the file on each request/responce cycle,
+# neither of which would be suitable for responding keystroke by keystroke.
+
+WORDS = Vocab(CONFIG.VOCAB)
+
+###
+# Pages
+###
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    """The main page of the application"""
+    flask.g.vocab = WORDS.as_list()
+    flask.session["target_count"] = min(
+        len(flask.g.vocab), CONFIG.SUCCESS_AT_COUNT)
+    flask.session["jumble"] = jumbled(
+        flask.g.vocab, flask.session["target_count"])
+    flask.session["matches"] = []
+    app.logger.debug("Session variables have been set")
+    assert flask.session["matches"] == []
+    assert flask.session["target_count"] > 0
+    app.logger.debug("At least one seems to be set correctly")
+    return flask.render_template('vocab_init.html')
+
+
+@app.route("/_check")
+def countem():
+    text = flask.request.args.get("text", type=str)
+    jumble = flask.session["jumble"]
+    matches = flask.session.get("matches", [])  # Default to empty list
+
+    # Is it good?
+    word = ''
+    wrong_letter = ''
+    for i in text:
+        if LetterBag(jumble).contains(i):
+            word += i.upper()
+        else:
+            word += i.lower()
+            wrong_letter += i
+    matched = WORDS.has(text)
+
+    # Respond appropriately
+    if matched and wrong_letter == '' and not (text in matches):
+        # Cool, they found a new word
+        print("0")
+        matches.append(text)
+        flask.session["matches"] = matches
+        print(matches)
+        if len(matches) >= flask.session["target_count"]:
+            res = {"cond": 3,
+                   "message": matches,
+                   "words": word}
+        else:
+            res = {"cond": 0,
+                   "message": matches,
+                   "words": word}
+    elif wrong_letter != '':
+        # Cool, they found a new word
+        print("1")
+        res = {"cond": 1,
+               "message": '"{}" is invalid'.format(wrong_letter),
+               "words" : word}
+    else:
+        print("2")
+        res = {"cond": 2,
+               "words": word}
+
+    return flask.jsonify(result=res)
+
+
+@app.route("/keep_going")
+def keep_going():
+    """
+    After initial use of index, we keep the same scrambled
+    word and try to get more matches
+    """
+    flask.g.vocab = WORDS.as_list()
+    return flask.render_template('vocab_init.html')
+
+
+@app.route("/success")
+def success():
+    return flask.render_template('success.html')
+
+#######################
+# Form handler.
+# CIS 322 note:
+#   You'll need to change this to a
+#   a JSON request handler
+#######################
+
+
+###############
+# AJAX request handlers
+#   These return JSON, rather than rendering pages.
+###############
+
+
+@app.route("/_example")
+def example():
+    """
+    Example ajax request handler
+    """
+    app.logger.debug("Got a JSON request")
+    rslt = {"key": "value"}
+    return flask.jsonify(result=rslt)
+
+
+#################
+# Functions used within the templates
+#################
+
+@app.template_filter('filt')
+def format_filt(something):
+    """
+    Example of a filter that can be used within
+    the Jinja2 code
+    """
+    return "Not what you asked for"
+
+###################
+#   Error handlers
+###################
+
+
+@app.errorhandler(404)
+def error_404(e):
+    app.logger.warning("++ 404 error: {}".format(e))
+    return flask.render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def error_500(e):
+    app.logger.warning("++ 500 error: {}".format(e))
+    assert not True  # I want to invoke the debugger
+    return flask.render_template('500.html'), 500
+
+
+@app.errorhandler(403)
+def error_403(e):
+    app.logger.warning("++ 403 error: {}".format(e))
+    return flask.render_template('403.html'), 403
+
+
+####
+
+if __name__ == "__main__":
+    if CONFIG.DEBUG:
+        app.debug = True
+        app.logger.setLevel(logging.DEBUG)
+        app.logger.info(
+            "Opening for global access on port {}".format(CONFIG.PORT))
+        app.run(port=CONFIG.PORT, host="0.0.0.0")
